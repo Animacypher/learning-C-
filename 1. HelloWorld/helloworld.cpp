fe5be817c69ce327662ad1ffbc36c871dfbe284a
Animacypher <sj2404629@gmail.com>
2026-08-15 18:15:50 +0800

    first commit

---
+++ 1. HelloWorld/helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    std::cout << "I'm Samuel John" << "\n";
+    std::cout << "I need a job" << "\n";
+    std::cout << "I'm broke and lonely" << "\n";
+    return 0;
+}

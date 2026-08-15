ca0fb40f877c17e3e4b235c1c1ea8cd83981c23b
Animacypher <sj2404629@gmail.com>
2026-08-15 22:49:09 +0800

    Day 1

--- 01. HelloWorld/helloworld.cpp
+++ 01. HelloWorld/helloworld.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
 int main(){
+
+    // This is a comment, ignored, just like me.....
+    /*
+    Multi line comment
+    All these lines are meaningless
+    if she's not meant for you.....
+    */
+
     std::cout << "I'm Samuel John" << "\n";
     std::cout << "I need a job" << "\n";
     std::cout << "I'm broke and lonely" << "\n";
---
+++ 02. Variables/variable.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main(){
+
+    // Integer (Whole Number)
+    int x = 6;
+    int y = 7;
+    std::cout << x << y << "\n"; //hehehehehe
+
+    //double (number including decimal)
+    double gpa = 3.5; //One day, gang
+    std::cout << gpa << "\n"; 
+
+    // Single Character
+    char currency = '$';
+    std::cout << "I need " << currency << "1000" << "\n";
+
+    // Boolean
+    bool areYouGay = true;
+
+    // String
+    std::string areYou = "You are";
+    std::string sad = "Gay";
+
+    std::cout << areYou << " " << sad << "\n";
+
+    return 0;
+}
